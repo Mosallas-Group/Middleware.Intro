3837eb62ffd8c473730e87180185bd56cde60d03
srezasm <srezas.2017@gmail.com>
2021-03-11 18:51:51 +0330

    Middleware Intro Completed

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using MosallasGroup.Middleware.Intro.Models;
+
+namespace MosallasGroup.Middleware.Intro.Controllers
+{
+   public class HomeController : Controller
+   {
+      private readonly ILogger<HomeController> _logger;
+
+      public HomeController(ILogger<HomeController> logger)
+      {
+         _logger = logger;
+      }
+
+      public IActionResult Index()
+      {
+         //return View();
+
+         throw new Exception("unhandled exception");
+      }
+
+      public IActionResult Privacy()
+      {
+         return View();
+      }
+
+      [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+      public IActionResult Error()
+      {
+         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+      }
+   }
+}
